7da122fd45ef2e376822550039a8f42a534fa12c
Ken Soh <kensoh@gmail.com>
2019-03-31 01:56:47 +0800

    initial commit - Placeholder for TagUI Python package

---
+++ README.txt
@@ -0,0 +1,5 @@
+=====
+tagui
+=====
+
+Placeholder for TagUI Python package - a wrapper for https://github.com/kelaberetiv/TagUI
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='tagui',
+    version='0.0.1',
+    author='Ken Soh',
+    author_email='ken@tebel.org',
+    url='http://tebel.org',
+    license='LICENSE.txt',
+    description='Placeholder for TagUI Python package',
+    long_description=open('README.txt').read(),
+)
