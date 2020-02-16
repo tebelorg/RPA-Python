60b8c7e76c7f2e48ce516e72c6e34fe33d0364d9
Ken Soh <kensoh@gmail.com>
2020-02-16 12:23:09 +0800

    #103 - add secondary chrome location for windows

--- rpa_package/rpa.py
+++ rpa_package/rpa.py
@@ -2,7 +2,7 @@
 # Apache License 2.0, Copyright 2020 Tebel.Automation Private Limited
 # https://github.com/tebelorg/RPA-Python/blob/master/LICENSE.txt
 __author__ = 'Ken Soh <opensource@tebel.org>'
-__version__ = '1.24.1'
+__version__ = '1.25.0'
 
-# for backward compatibility, invoke functions from tagui.py
+# for backward compatibility, invoke tagui.py functions to use in rpa.py
 from tagui import *
