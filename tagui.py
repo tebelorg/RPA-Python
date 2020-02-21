ee1c0f019f26d825f37b85d14e3d981e5409b2a3
Ken Soh <kensoh@gmail.com>
2020-02-21 11:20:14 +0800

    #101 - exit Chrome cleanly on Windows

--- tagui.py
+++ tagui.py
@@ -2,7 +2,7 @@
 # Apache License 2.0, Copyright 2020 Tebel.Automation Private Limited
 # https://github.com/tebelorg/RPA-Python/blob/master/LICENSE.txt
 __author__ = 'Ken Soh <opensource@tebel.org>'
-__version__ = '1.25.0'
+__version__ = '1.26.0'
 
 import subprocess
 import os
